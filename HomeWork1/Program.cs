c49b1df109552493daca4dd381f96b4d5335b596
Svetlana <Khubezheva@gmail.com>
2022-08-06 16:04:48 +0300

    домашняя работа 1

---
+++ HomeWork1/Program.cs
@@ -0,0 +1,79 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+// Console.Write("Введите первое число: ");
+// int firstNumber = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число: ");
+// int secondNumber = Convert.ToInt32(Console.ReadLine());
+// if (firstNumber > secondNumber * secondNumber)
+// {
+//     Console.WriteLine("max = " + firstNumber);
+// }
+// else
+// {
+//     Console.WriteLine("max = " + secondNumber);
+// }
+
+
+
+
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+// Console.Write("Введите первое число: ");
+// int firstNumber = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число: ");
+// int secondNumber = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите третье число: ");
+// int thirdNumber = Convert.ToInt32(Console.ReadLine());
+// int max = firstNumber;
+// if (secondNumber > max) max = secondNumber;
+// if (thirdNumber > max) max = thirdNumber;
+// Console.WriteLine("max = " + max);
+
+
+
+
+// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+// Console.Write("Введите число: ");
+// int Number = Convert.ToInt32(Console.ReadLine());
+// if (Number %2 == 0)
+// {
+//     Console.WriteLine("Да");
+// }
+// else
+// {
+//     Console.WriteLine("Нет");
+// }
+
+
+
+
+// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Write("Введите число: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+if (Number > 1)
+{
+
+    int Number1 = 2;
+    while (Number1 <= Number)
+    {
+        Console.WriteLine(Number1);
+        Number1 = Number1 + 2;
+    }
+}
+else
+{
+    Console.WriteLine("Eror 404");
+}    
---
+++ Seminar_1/Program.cs
@@ -0,0 +1,42 @@
+﻿// int number = Convert.ToInt32(Console.ReadLine());
+// int square = number * number;
+// Console.WriteLine("Результат: " + square);
+
+
+
+// Console.Write("Введите первое число: ");
+// int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите второе число: ");
+// int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+// if (firstNumber == secondNumber * secondNumber)
+// {
+//     Console.WriteLine(firstNumber + " явялется квадратом от " + secondNumber);
+// }
+// else
+// {
+//     Console.WriteLine(firstNumber + " НЕ явялется квадратом от " + secondNumber);
+// }
+
+
+// int DayNumber = Convert.ToInt32(Console.ReadLine());
+// if (DayNumber == 1)
+// {
+//     Console.WriteLine("Понедельник");
+// }
+// и т.д.
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number<0)
+{
+    number = number * (-1);
+}
+int negativNumber = number * (-1);
+
+while (negativNumber <= number)
+{
+    Console.WriteLine(negativNumber);
+    negativNumber++; //++ увеличить на единицу
+}
