1b8dcaa92053cbb3279fa2531bc76b53a2b4b12b
Svetlana <Khubezheva@gmail.com>
2022-08-16 16:03:22 +0300

    домашняя работа 2

--- HomeWork2/Program.cs
+++ HomeWork2/Program.cs
@@ -19,7 +19,16 @@
 
 Console.Write("Введите число: ");
 string number = Console.ReadLine();
-int n = array.Length;
-if (n > 2) Console.WriteLine($"третья цифра:{number[2]}");
+if(number.Length >= 3) Console.WriteLine($"третья цифра:{number[2]}");
+else Console.WriteLine("Третьей цифры нет");
+
+// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
 
 
+// Console.Write("Введите число от 1 до 7: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// if (num == 6 || num == 7) Console.WriteLine("да");
+// else Console.WriteLine("нет");
