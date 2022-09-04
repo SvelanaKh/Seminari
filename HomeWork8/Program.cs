5e3e8489113c0af1018280b131f48d1247c94fa7
Svetlana <Khubezheva@gmail.com>
2022-09-04 18:43:50 +0300

    домашняя работа 8

---
+++ HomeWork8/Program.cs
@@ -0,0 +1,251 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+
+// Console.WriteLine("введите количество строк");
+// int rows = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("введите количество столбцов");
+// int columns = Convert.ToInt32(Console.ReadLine());
+
+// int[,] numbers = new int[rows, columns];
+
+// void FillArray(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             array[i, j] = new Random().Next(0, 10);
+//         }
+//     }
+// }
+
+// void PrintArray(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write(array[i, j] + "\t");
+//         }
+//         Console.WriteLine("");
+//     }
+// }
+// FillArray(numbers);
+// Console.WriteLine();
+
+// PrintArray(numbers);
+
+// for (int i = 0; i < numbers.GetLength(0); i++)
+// {
+//     for (int j = 0; j < numbers.GetLength(1); j++)
+//     {
+//         for (int k = 0; k < numbers.GetLength(1) - 1; k++)
+//         {
+//             if (numbers[i, k] < numbers[i, k + 1]) 
+//             {
+//                 int temp = numbers[i, j];
+//                 numbers[i, k] = numbers[i, k + 1];
+//                 numbers[i, k + 1] = temp;
+//             }
+//         }
+//     }
+// }
+// Console.WriteLine();
+// Console.WriteLine("Массив с упорядоченными значениями");
+// PrintArray(numbers);
+
+
+
+// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с 
+// наименьшей суммой элементов.
+// Например, задан массив:
+
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+// Console.WriteLine("введите количество строк");
+// int rows = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("введите количество столбцов");
+// int columns = Convert.ToInt32(Console.ReadLine());
+
+// int[,] numbers = new int[rows, columns];
+
+// void FillArray(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             array[i, j] = new Random().Next(0, 10);
+//         }
+//     }
+// }
+
+// void PrintArray(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write(array[i, j] + "\t");
+//         }
+//         Console.WriteLine("");
+//     }
+// }
+// FillArray(numbers);
+// Console.WriteLine();
+
+// PrintArray(numbers);
+
+
+// int minsum = Int32.MaxValue;
+// int numberLine = 0;
+
+// for (int i = 0; i < numbers.GetLength(0); i++)
+// {
+//     int sum = 0;
+//     for (int j = 0; j < numbers.GetLength(1); j++)
+//     {
+//         sum = sum + numbers[i, j];        
+//     }
+//     if (sum < minsum)
+//     {
+//         minsum = sum;
+//         numberLine = i + 1;
+//     }
+
+// }
+// Console.WriteLine($"номер строки с наименьшей суммой елементов:  {numberLine} , с суммой елементов равной:  {minsum}");
+
+
+
+
+// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+
+// Console.WriteLine("введите количество строк");
+// int rows = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("введите количество столбцов");
+// int columns = Convert.ToInt32(Console.ReadLine());
+
+// int[,] matrixA = new int[rows, columns];
+// int[,] matrixB = new int[rows, columns];
+
+// void FillArray(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             array[i, j] = new Random().Next(0, 10);
+//         }
+//     }
+// }
+
+// void PrintArray(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write(array[i, j] + "\t");
+//         }
+//         Console.WriteLine("");
+//     }
+// }
+// FillArray(matrixA);
+// FillArray(matrixB);
+// Console.WriteLine("Матрица А");
+// PrintArray(matrixA);
+// Console.WriteLine();
+// Console.WriteLine("Матрица В");
+// PrintArray(matrixB);
+// Console.WriteLine();
+
+// int[,] matrixC = new int[rows, columns];
+
+// for (int i = 0; i < matrixC.GetLength(0); i++)
+// {
+//     for (int j = 0; j < matrixC.GetLength(1); j++)
+//     {
+//         for (int k = 0; k < matrixC.GetLength(0); k++)
+//         {
+//             matrixC[i, j] = matrixC[i, j] + (matrixA[i, k] * matrixB[k, j]);
+//         }
+//     }
+// }
+// Console.WriteLine("Произведение");
+// PrintArray(matrixC);
+
+
+
+// Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, 
+// которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+
+
+Console.WriteLine("введите размерность 1");
+int dimensions1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите размерность 2");
+int dimensions2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите размерность 3");
+int dimensions3 = Convert.ToInt32(Console.ReadLine());
+
+int[,,] matrix3D = new int[dimensions1, dimensions2, dimensions3];
+void FillArray3D(int[,,] array)
+
+{
+    int start = 10;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                array[i, j, k] = start;
+                start++;
+            }
+        }
+    }
+}
+FillArray3D(matrix3D);
+
+void PrintArray(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                Console.WriteLine($"{array[i, j, k]}({i},{j},{k})");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+}
+
+PrintArray(matrix3D);
---
+++ Seminar_8/Program.cs
@@ -0,0 +1,277 @@
+﻿//Задача 53. Задайте двумерный массив. Напишите программу, которая поменяет местами первую и последнюю строку массива
+
+
+// Console.Write("Введите количество строк: ");
+// int rows = Convert.ToInt32(Console.ReadLine()!);
+// Console.Write("Введите количество столбцов: ");
+// int columns = Convert.ToInt32(Console.ReadLine()!);
+
+// int[,] matrix = new int[rows, columns];
+// for (int i = 0; i < matrix.GetLength(0); i++)
+// {
+//     // i, j, m, k
+//     for (int j = 0; j < matrix.GetLength(1); j++)
+//     {
+//         matrix[i, j] = new Random().Next(11);
+//     }
+// }
+// void ChangeLines(int[,] matrix)
+// {
+//     int lastIndex = matrix.GetLength(0);
+//     for (int j = 0; j < lastIndex; j++)
+//     {
+//         int temp = matrix[0, j];
+//         matrix[0, j] = matrix[lastIndex - 1, j];
+//         matrix[lastIndex - 1, j] = temp;
+//     }
+// }
+// void PrintArray(int[,] inputMatrix)
+// {
+//     for (int i = 0; i < inputMatrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < inputMatrix.GetLength(1); j++)
+//         {
+//             Console.Write(inputMatrix[i, j] + "\t"); // "\t" - Tab
+//         }
+//         Console.WriteLine();
+//     }
+// }
+// PrintArray(matrix);
+// ChangeLines(matrix);
+// Console.WriteLine();
+// PrintArray(matrix);
+
+
+
+// Задача 55: Задайте двумерный массив. Напишите программу,
+// которая заменяет строки на столбцы. В случае, если это не
+// возможно, программа должна вывести сообщение для
+// пользователя.
+
+// Console.WriteLine("Введите колличество строк: ");
+// int rows = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите колличество столбцов: ");
+// int columns = Convert.ToInt32(Console.ReadLine());
+
+// int[,] MakeRandArray(int m, int n)
+// {
+//     int[,] matrix = new int[m, n];
+//     for (int i = 0; i < m; i++)
+//     {
+//         for (int j = 0; j < n; j++)
+//         {
+//             matrix[i, j] = new Random().Next(10);
+//         }
+//     }
+//     return matrix;
+// }
+
+// void TurnOverArray(int[,] matrix, int m, int n)// можно войд
+// {
+//     for (int j = 0; j < m; j++)
+//     {
+//         for (int i = j; i < n; i++)
+//         {
+//             int row = matrix[j, i];
+//             matrix[j, i] = matrix[i, j];
+//             matrix[i, j] = row;
+//         }
+//     }
+// }
+
+// int[,] MakeTurnOverArray(int[,] oldMatrix, int m, int n)
+// {
+//     int[,] matrix = new int[n, m];
+//     for (int i = 0; i < m; i++)
+//     {
+//         for (int j = 0; j < n; j++)
+//         {
+//             matrix[j, i] = oldMatrix[i, j];
+//         }
+//     }
+//     return matrix;
+// }
+
+// void PrintArray(int[,] matrix, int m, int n)
+// {
+//     Console.WriteLine("\n\n");
+//     for (int i = 0; i < m; i++)
+//     {
+//         for (int j = 0; j < n; j++)
+//         {
+//             Console.Write($"{matrix[i, j]}\t");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// int[,] Array = MakeRandArray(rows, columns);
+// PrintArray(Array, rows, columns);
+
+// if (rows == columns)
+// {
+//     TurnOverArray(Array, rows, columns);
+//     PrintArray(Array, rows, columns);
+// }
+// else
+// {
+//     int[,] turnOverArray = MakeTurnOverArray(Array, rows, columns);
+//     PrintArray(turnOverArray, columns, rows);//интересный момент нужно написать строки и столбцы наоборот потому что массив у нас на боку: {Ленивый массив}
+// }
+
+
+// Задача 57: Составить частотный словарь элементов двумерного
+// массива. Частотный словарь содержит информацию о том,
+// сколько раз встречается элемент входных данных.
+
+Console.Clear();
+
+Console.WriteLine("Введите максимальное диапазона: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите минимальное диапазона: ");
+int min = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите колличество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите колличество столбцов: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int[,] MakeRandArray(int rows, int columns, int max, int min)
+{
+    int[,] matrix = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            matrix[i, j] = new Random().Next(min, max);
+        }
+    }
+    return matrix;
+}
+
+void FindCountAndPrint(int[,] matrix, int rows, int columns, int max, int min)
+{
+
+    Console.WriteLine();
+    for (int i = 0; i < max - min; i++)
+    {
+        int count = 0;
+        for (int j = 0; j < rows; j++)
+        {
+            for (int k = 0; k < columns; k++)
+            {
+                if (matrix[j, k] == min + i) count++;
+            }
+        }
+        Console.WriteLine($"{min + i}: встречается {count} раз");
+    }
+}
+
+void PrintArray(int[,] matrix, int m, int n)
+{
+    Console.WriteLine("\n\n");
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            Console.Write($"{matrix[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] Array = MakeRandArray(rows, columns, max, min);
+PrintArray(Array, rows, columns);
+FindCountAndPrint(Array, rows, columns, max, min);
+
+
+// Задача 59: Задайте двумерный массив из целлых чисел.
+// Напишите программу, которая удалит строку и столбец, на
+// пересечении которых расположен наименьший элемент массива
+
+Console.Clear();
+
+Console.Write("Введите колличество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите колличество столбцов: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int[,] MakeRandArray(int rows, int columns)
+{
+    int[,] matrix = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            matrix[i, j] = new Random().Next(10);
+        }
+    }
+    return matrix;
+}
+
+int FindMin(int[,] matrix, int rows, int columns)
+{
+    int min = 2147483647;//Костыль!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            if (matrix[i, j] < min) min = matrix[i, j];
+        }
+    }
+    return min;
+}
+
+void PrintArrayAndDeleteRowAndColumn(int[,] matrix, int m, int n, int min)
+{
+    Console.WriteLine("\n\n");
+    if (min == -2147483648)
+    {
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                Console.Write($"{matrix[i, j]}\t");
+            }
+            Console.WriteLine();
+        }
+    }
+    else
+    {
+        int iMin = 0;
+        int jMin = 0;
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (min == matrix[i, j])
+                {
+                    iMin = i;
+                    jMin = j;
+                    goto firstMinFound;
+                }
+            }
+        }
+    firstMinFound:
+        Console.WriteLine($"Min = {min}[{iMin}, {jMin}]");
+        Console.WriteLine();
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (i != iMin
+                && j != jMin) Console.Write($"{matrix[i, j]}\t");
+            }
+            if (i != iMin) Console.WriteLine();
+        }
+    }
+}
+
+int[,] Array = MakeRandArray(rows, columns);
+PrintArrayAndDeleteRowAndColumn(Array, rows, columns, -2147483648);// число -11567 это костыль
+int min = FindMin(Array, rows, columns);
+PrintArrayAndDeleteRowAndColumn(Array, rows, columns, min);
